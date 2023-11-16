edb218bda15871afc3e6ecf7da783d32038f7cc7
epaalbaik <revalcedia12@gmail.com>
2023-11-16 13:08:40 +0700

    pertemuan_1

---
+++ pertemuan_1/Hallo_World.php
@@ -0,0 +1,20 @@
+<?php
+   $nama="Reval Putra Cedia";
+?>
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Latihan 1</title>
+     </head>
+     <body>
+          <center>
+               <h2><b>halo</b></h2>
+               <h1><b>Selamat datang di pemograman dasar</b></h1>
+               <?php
+               echo $nama;
+               ?>
+          </center>
+     </body>
+</html>
---
+++ pertemuan_1/volume_bola.php
@@ -0,0 +1,6 @@
+<?php
+$jari_jari = 7;
+$volume = (4/3)* 3.14* $jari_jari*$jari_jari*$jari_jari;
+
+echo "jika jari-jari dari bola adalah $jari_jari maka volume bola adalah $volume";
+?>
